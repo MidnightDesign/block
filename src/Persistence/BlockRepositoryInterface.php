d1c235745db4bf013e923bdfe0c7bb47b4e88523
Rudolph Gottesheim <r.gottesheim@loot.at>
2018-01-12 20:52:33 +0100

    Add persistence interfaces

---
+++ src/Persistence/BlockRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Midnight\Block\Persistence;
+
+use Midnight\Block\BlockInterface;
+
+interface BlockRepositoryInterface
+{
+    /**
+     * @throws Exception\UnknownBlockException
+     */
+    public function findById(): BlockInterface;
+}
---
+++ src/Persistence/Exception/UnknownBlockException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Midnight\Block\Persistence\Exception;
+
+use RuntimeException;
+
+class UnknownBlockException extends RuntimeException
+{
+}
